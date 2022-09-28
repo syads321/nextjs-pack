3a344763c7e427539d2ffe90bda65b510ca770c5
syads3211@gmail.com <syads321@gmail.com>
2022-09-29 06:41:50 +0700

    add rich editor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
+import '@/styles/icons.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
 
